0048ea003ffe6323885ee29a26c201f702192ac8
Jaeha Hwang <wogk216@naver.com>
2023-07-25 21:07:23 +0900

    EmotionApp앱생성

---
+++ EmotionApp/EmotionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionApp
+//
+//  Created by 황재하 on 7/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
