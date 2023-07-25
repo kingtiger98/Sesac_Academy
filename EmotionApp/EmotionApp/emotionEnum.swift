b14bde96dfb6e326607adcf9d2eacdbcb1049e70
Jaeha Hwang <wogk216@naver.com>
2023-07-25 22:05:24 +0900

    EmotionApp_UI완성

---
+++ EmotionApp/EmotionApp/emotionEnum.swift
@@ -0,0 +1,16 @@
+//
+//  emotionEnum.swift
+//  EmotionApp
+//
+//  Created by 황재하 on 7/25/23.
+//
+
+import Foundation
+
+enum Emotion {
+    case pink
+    case orange
+    case yellow
+    case mint
+    case blue
+}
