317904a6665aebba844c9fc35066f7c056b6b768
Jaeha Hwang <wogk216@naver.com>
2023-08-13 12:34:48 +0900

    API키 암호화 확인중

--- TMDBProject/TMDBProject/APIKey.swift
+++ TMDBProject/TMDBProject/APIKey.swift
@@ -11,3 +11,6 @@ enum APIKey {
     static let TMDBKey: String = "c0b4546420c726a81d2b711a048e1846&language=ko-KR"
     static let TMDBToken: String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGI0NTQ2NDIwYzcyNmE4MWQyYjcxMWEwNDhlMTg0NiIsInN1YiI6IjY0ZDZlZGU3ZjQ5NWVlMDI4ZjYzZDNiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BGtl-e-IpDeMJ9JAqP2qK5rmfvOf5gBPOuuYeYfDcbk"
 }
+
+// 숨겨진거니?
+
